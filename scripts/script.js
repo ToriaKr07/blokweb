d2a6ec72aa57ba720a383648067e34dbbd8c2ff3
ToriaKr07 <toria.kruithof@hva.nl>
2025-09-22 10:56:24 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,24 @@
 // JavaScript Document
 console.log("hi");
+
+let openMenuButton = document.querySelector("header > nav > button")
+let navTonen = document.querySelector("header nav:nth-of-type(2)");
+
+// stap 2: laat de menu-button luisteren naar kliks en voer dan een functie uit
+openMenuButton.addEventListener("click", openMenu);
+
+// stap 3: voeg in de functie een class toe aan de nav
+function openMenu() {
+  navTonen.classList.add("is-open");
+}
+
+let sluitMenuButton = document.querySelector("header nav:nth-of-type(2) button")
+
+// stap 2 - laat die button luisteren naar kliks
+sluitMenuButton.addEventListener("click", sluitMenu);
+
+// stap 3 - in de functie verwijder de class van de nav
+function sluitMenu() {
+  let navTonen = document.querySelector("header nav:nth-of-type(2)");
+  navTonen.classList.remove("is-open");
+}
