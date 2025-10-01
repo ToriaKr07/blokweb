f61b8adfa411a4d6bb37b7cd95139d04d4c1d759
ToriaKr07 <toria.kruithof@hva.nl>
2025-10-01 17:46:48 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -24,49 +24,6 @@ function sluitMenu() {
 }
 
 
-
-// Zoek elementen op de juiste manier
-// let zoekbalkTonen = document.querySelector(".search");  // of #search
-// let menuOpenen = document.querySelector(".search button");
-// let menuSluiten = document.querySelector(".search .close");
-
-// // Open zoekbalk
-// menuOpenen.addEventListener("click", function() {
-//   zoekbalkTonen.classList.add("zoeken");
-// });
-
-// // Sluit zoekbalk
-// menuSluiten.addEventListener("click", function() {
-//   zoekbalkTonen.classList.remove("zoeken");
-// });
-
-
-
-// let menuOpenen = document.querySelector(".search button")
-// let zoekbalkTonen = document.querySelector(".search");
-
-// // stap 2: laat de menu-button luisteren naar kliks en voer dan een functie uit
-// menuOpenen.addEventListener("click", openMenu);
-
-// // stap 3: voeg in de functie een class toe aan de nav
-// function openMenu() {
-//   zoekbalkTonen.classList.add("zoeken");
-// }
-
-// let menuSluiten = document.querySelector("header .search button")
-
-// // stap 2 - laat die button luisteren naar kliks
-// menuSluiten.addEventListener("click", sluitMenu2);
-
-// // stap 3 - in de functie verwijder de class van de nav
-// function sluitMenu2() {
-//   let zoekbalkTonen = document.querySelector(".search");
-//   zoekbalkTonen.classList.remove("zoeken");
-// }
-
-
-
-
 let videoButton = document.querySelector("section:nth-of-type(3) button");
 let videoButtonImg = document.querySelector("section:nth-of-type(3) button img");
 let deVideo = document.querySelector("video");
